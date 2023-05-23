3a2d790fb16a719775068a2ed0b2f1822c603149
asya488 <anastasiia.pershyna@nure.ua>
2023-05-23 20:11:21 +0300

    more changes in bottom_nav_menu started making top menu

--- app/src/main/java/com/example/allergydotnet/MapActivity.kt
+++ app/src/main/java/com/example/allergydotnet/MapActivity.kt
@@ -1,11 +1,16 @@
 package com.example.allergydotnet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        val btmNav = findViewById<BottomNavigationView>(R.id.bottom_navigation);
+        btmNav.itemIconTintList = null;
     }
 }
