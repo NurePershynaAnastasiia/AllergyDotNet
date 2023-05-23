3e8470d05f829d2a4618873ae54b359843856633
asya488 <anastasiia.pershyna@nure.ua>
2023-05-23 22:08:16 +0300

    frontend for NewNotationActivity

---
+++ app/src/main/java/com/example/allergydotnet/NewNotationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.allergydotnet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewNotationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_notation)
+    }
+}
