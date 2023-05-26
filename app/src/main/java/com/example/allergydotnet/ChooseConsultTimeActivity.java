8590ee417bbac02449d7443f37d9ed0e6f3d63ec
asya488 <anastasiia.pershyna@nure.ua>
2023-05-27 01:44:06 +0300

    created base layouts for allergen_details and choose_consult_time

---
+++ app/src/main/java/com/example/allergydotnet/ChooseConsultTimeActivity.java
@@ -0,0 +1,14 @@
+package com.example.allergydotnet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseConsultTimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_consult_time);
+    }
+}
