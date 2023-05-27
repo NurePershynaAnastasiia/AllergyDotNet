066596afe035521befd2a19e90231c8d35992e96
eliza <eliza@DESKTOP-991N2IR>
2023-05-27 03:27:55 +0300

    choose_consut (.xml) changed created activity for te test

---
+++ app/src/main/java/com/example/allergydotnet/TestsActivity.java
@@ -0,0 +1,68 @@
+package com.example.allergydotnet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class TestsActivity extends AppCompatActivity {
+RelativeLayout layout;
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tests);
+        layout = findViewById(R.id.relative);
+        button = findViewById(R.id.test_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreatepopUpwindow();
+            }
+        });
+    }
+
+    private void CreatepopUpwindow() {
+        LayoutInflater inflater= (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popUpView=inflater.inflate(R.layout.popup_standart,null);
+
+        int width = ViewGroup.LayoutParams.MATCH_PARENT;
+        int height = ViewGroup.LayoutParams.MATCH_PARENT;
+        boolean focusable = true;
+        PopupWindow popupWindow = new PopupWindow(popUpView,width,height,focusable);
+        layout.post(new Runnable() {
+            @Override
+            public void run() {
+                popupWindow.showAtLocation(layout, Gravity.BOTTOM,0,0);
+
+            }
+        });
+        TextView Skip ,Back;
+
+        Back = popUpView.findViewById(R.id.backbtn);
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // write code anything you want
+            }
+        });
+
+        // and if you want to close popup when touch Screen
+        popUpView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    }
+}
