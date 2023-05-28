7ad56a3e33e6e201f94b8c3ebe097b433c58987d
Anastasiia <nastysha.001.99@gmail.com>
2023-05-28 23:49:38 +0300

    demo server 1.2 routing

--- serverDemo.js
+++ serverDemo.js
@@ -1,10 +1,42 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, World!');
+    console.log('Server request');
+
+    res.setHeader('Content-Type', 'text/html');
+
+    const createPath = (page) => path.resolve(__dirname, 'HTMLPagesDemo', `${page}.html`);
+    let basePath = '';
+
+    switch(req.url) {
+        case '/FormDemo.html':
+            basePath = createPath('FormDemo');
+            res.statusCode = 200;
+            break;
+        case '/AnswerDemo':
+            basePath = createPath('FormDemo');
+            res.statusCode = 200;
+            break;
+        default:
+            basePath = createPath('error');
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(basePath, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.statusCode = 500;
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
 });
 
 server.listen(PORT, 'localhost', (error) => {
