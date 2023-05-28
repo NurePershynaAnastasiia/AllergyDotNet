a199114c9bd612c7f0a7a3c11265efbfa2febb72
eliza <eliza@DESKTOP-991N2IR>
2023-05-29 00:26:43 +0300

    popup windows are modified and connected to the activities

--- app/src/main/java/com/example/allergydotnet/ProfileActivity.java
+++ app/src/main/java/com/example/allergydotnet/ProfileActivity.java
@@ -3,19 +3,26 @@ package com.example.allergydotnet;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class ProfileActivity extends AppCompatActivity {
-
+RelativeLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +57,49 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        RelativeLayout subscription_layout = findViewById(R.id.layout1_profile);
+        layout = findViewById(R.id.profile_relative);
+        subscription_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreatepopUpwindow();
+            }
+        });
+    }
 
+    private void CreatepopUpwindow() {
+        LayoutInflater inflater= (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popUpView=inflater.inflate(R.layout.popup_subscription,null);
+
+        int width = ViewGroup.LayoutParams.MATCH_PARENT;
+        int height = ViewGroup.LayoutParams.MATCH_PARENT;
+        boolean focusable = true;
+        PopupWindow popupWindow = new PopupWindow(popUpView,width,height,focusable);
+        layout.post(new Runnable() {
+            @Override
+            public void run() {
+                popupWindow.showAtLocation(layout, Gravity.BOTTOM,0,0);
+
+            }
+        });
+
+        Button chooseSubscription;
+        chooseSubscription = popUpView.findViewById(R.id.standartbtn);
+
+        chooseSubscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // write code anything you want
+            }
+        });
+
+        // and if you want to close popup when touch Screen
+        popUpView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 }
