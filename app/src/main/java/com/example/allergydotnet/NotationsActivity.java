145f98b20a5e303e2d5aee47a8708a95aed24cae
asya488 <anastasiia.pershyna@nure.ua>
2023-05-31 00:32:55 +0300

    added consultations' list frontend

--- app/src/main/java/com/example/allergydotnet/NotationsActivity.java
+++ app/src/main/java/com/example/allergydotnet/NotationsActivity.java
@@ -1,8 +1,17 @@
 package com.example.allergydotnet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class NotationsActivity extends AppCompatActivity {
 
@@ -10,5 +19,35 @@ public class NotationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notations);
+
+        RelativeLayout top_menu = findViewById(R.id.top_menu);
+        ImageButton toProfile = findViewById(R.id.profilebtn);
+        toProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myint = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(myint);
+            }
+        });
+
+        BottomNavigationView bottomNavMenu = findViewById(R.id.bottom_navigation);
+        bottomNavMenu.setSelectedItemId(R.id.invisible);
+        bottomNavMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent myint;
+                if (item.getItemId() == R.id.consultations) {
+                    myint = new Intent(getApplicationContext(), ConsultationsActivity.class);
+                    startActivity(myint);
+                } else if (item.getItemId() == R.id.map) {
+                    myint = new Intent(getApplicationContext(), MapActivity.class);
+                    startActivity(myint);
+                } else if (item.getItemId() == R.id.doctors) {
+                    myint = new Intent(getApplicationContext(), DoctorsActivity.class);
+                    startActivity(myint);
+                }
+                return false;
+            }
+        });
     }
 }
