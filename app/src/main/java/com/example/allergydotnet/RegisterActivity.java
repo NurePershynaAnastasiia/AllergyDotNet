d0b8b771ae94d4b6f68c7ff094b2bf0658fae26b
asya488 <anastasiia.pershyna@nure.ua>
2023-05-30 22:21:44 +0300

    added validation in register activity

--- app/src/main/java/com/example/allergydotnet/RegisterActivity.java
+++ app/src/main/java/com/example/allergydotnet/RegisterActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -15,12 +18,42 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+
+        EditText usernameEditText = findViewById(R.id.usernameEditText);
+        EditText emailEditText = findViewById(R.id.emailEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+        EditText repasswordEditText = findViewById(R.id.repasswordEditText);
         Button btn = findViewById(R.id.signupbtn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myint = new Intent(getApplicationContext(), ProfileActivity.class);
-                startActivity(myint);
+                String textName = usernameEditText.getText().toString();
+                String textEmail = emailEditText.getText().toString();
+                String textPassword = passwordEditText.getText().toString();
+                String textRepassword = repasswordEditText.getText().toString();
+
+                //Check if the fields aren't empty
+                if (TextUtils.isEmpty(textName)){
+                    Toast.makeText(RegisterActivity.this, "Будь ласка, введіть ім'я", Toast.LENGTH_SHORT).show();
+                    usernameEditText.setError("Ви не ввели ім'я");
+                    usernameEditText.requestFocus();
+                } else if (TextUtils.isEmpty(textEmail)){
+                    Toast.makeText(RegisterActivity.this, "Будь ласка, введіть пошту", Toast.LENGTH_SHORT).show();
+                    emailEditText.setError("Ви не ввели пошту");
+                    emailEditText.requestFocus();
+                } else if (TextUtils.isEmpty(textPassword)){
+                    Toast.makeText(RegisterActivity.this, "Будь ласка, введіть пароль", Toast.LENGTH_SHORT).show();
+                    passwordEditText.setError("Ви не ввели пароль");
+                    passwordEditText.requestFocus();
+                } else if (TextUtils.isEmpty(textRepassword) || !TextUtils.equals(textPassword, textRepassword)){
+                    Toast.makeText(RegisterActivity.this, "Будь ласка, підтвердіть пароль", Toast.LENGTH_SHORT).show();
+                    repasswordEditText.setError("Введені паролі не співпадають");
+                    repasswordEditText.requestFocus();
+                }
+                else {
+                    Intent myint = new Intent(getApplicationContext(), ProfileActivity.class);
+                    startActivity(myint);
+                }
             }
         });
 
