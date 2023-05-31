648a31803f5175ac9371bc5bfea8c8bd52cdfeaa
Anastasiia <nastysha.001.99@gmail.com>
2023-05-31 18:34:33 +0300

    demo server 2.7 logIn doc

--- DoctorLogIn.js
+++ DoctorLogIn.js
@@ -4,8 +4,6 @@ const fs = require('fs');
 const app = express();
 const bodyParser = require('body-parser');
 const db = new sqlite3.Database('AllergyDotNet.db');// Підключення до бази даних SQLite
-/*const query = `SELECT user_email, user_password FROM USERS
-                                 WHERE (user_email = $user_email AND user_password = $user_password)`;*/
 
 // Розбір даних у форматі JSON
 app.use(express.json());
@@ -14,26 +12,31 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-    fs.readFile('userLogin.json', 'utf8', async (err, data) => {
+    fs.readFile('doctorLogin.json', 'utf8', async (err, data) => {
         if (err) {
             return res.status(500).json({ error: 'Error reading the file' });
         }
         try {
-            const userData = JSON.parse(data);
-            db.get('SELECT user_id, user_password FROM Users WHERE user_email = ?', [userData.user_email], (err, row) => {
+            const doctorData = JSON.parse(data);
+            db.get('SELECT doctor_id, doctor_password, isAdmin FROM Doctors WHERE doctor_email = ?', [doctorData.doctor_email], (err, row) => {
                 if (err) {
                     console.error(err);
                     return res.status(500).send('Error retrieving data from the database');
                 }
 
                 if (row) {
-                    if (row.user_password === userData.user_password) {
-                        res.status(200).json({ user_id: row.user_id });
+                    if (row.doctor_password === doctorData.doctor_password) {
+                        if(!doctorData.isAdmin){
+                            res.status(200).json({ doctor_id: row.doctor_id, isAdmin: true });
+
+                        }else{
+                            res.status(200).json({ doctor_id: row.doctor_id, isAdmin: false });
+                        }
                     } else {
                         res.status(400).json({ error: 'Invalid password' });
                     }
                 } else {
-                    res.status(200).json({ user_id: false });
+                    res.status(200).json({ doctor_id: false });
                 }
             });
         } catch (error) {
