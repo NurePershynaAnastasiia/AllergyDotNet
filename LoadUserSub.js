e2b34d3771eec0520568563b9d1f81a2d5251fd1
malma <mariam.almakadma@nure.ua>
2023-06-01 21:42:03 +0200

    load user sub checked print

--- LoadUserSub.js
+++ LoadUserSub.js
@@ -12,15 +12,14 @@ app.use(bodyParser.json());
 
 app.post('/loadUserSub', (req, res) => {
     const user_id = req.body.user_id; // Отримання user_id з тіла запиту
-    const query = 'SELECT user_sub, FROM Users WHERE Users.user_id = ?;';
+    const query = 'SELECT user_sub FROM Users WHERE Users.user_id = ?';
 
-    db.get(query, [user_id], (err, rows) => { // Використовуйте db.all замість db.each для отримання всіх рядків
+    db.get(query, [user_id], (err, row) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Error retrieving notes from the database');
         }
-        res.status(200).json({user_sub: row});
-        //res.status(200).json(row);// я не знаю, как лучше, надо потестить!!!!!!!!!!!!!!!!!!!!!!!!
+        res.status(200).json(row);
     });
 });
 
