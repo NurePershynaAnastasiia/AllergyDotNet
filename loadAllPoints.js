cafcc8d1e41581e800e24451bf9e5d5b58f52221
Anastasiia <nastysha.001.99@gmail.com>
2023-06-01 23:44:41 +0300

    demo server 4.3 load all points completed

---
+++ loadAllPoints.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+const app = express();
+const bodyParser = require('body-parser');
+const db = new sqlite3.Database('AllergyDotNet.db');
+
+// Parse request bodies
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//
+app.post('/loadAllPoints', (req, res) => {
+    const query = 'SELECT point_name, point_photo, point_coordinates_latitude, point_coordinates_longitude FROM Points WHERE point_status = 1';
+
+    db.all(query,  (err, rows) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error retrieving doctors from the database');
+        }
+        res.json(rows);
+    });
+
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
