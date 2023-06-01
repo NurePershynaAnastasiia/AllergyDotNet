e69d16d0bbc6d54532643c00cb3cad4a4fd62b22
eliza <eliza@DESKTOP-991N2IR>
2023-06-01 15:50:26 +0300

    added code for app&sever interaction

--- app/src/main/java/com/example/allergydotnet/LoginActivity.java
+++ app/src/main/java/com/example/allergydotnet/LoginActivity.java
@@ -11,17 +11,36 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.allergydotnet.util.LoginInfo;
+import com.example.allergydotnet.util.RetrofitInterface;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.converter.gson.GsonConverterFactory;
+import java.util.HashMap;
+
+import retrofit2.Retrofit;
+
+public class LoginActivity extends AppCompatActivity {
+    private Retrofit retrofit;
+    private RetrofitInterface retrofitInterface;
+    private String BASE_URL = "http://localhost:3000/userLogIn/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        EditText emailEditText = findViewById(R.id.emailEditText);
-        EditText passwordEditText = findViewById(R.id.passwordEditText);
-
-        Button btn = findViewById(R.id.signupbtn);
+    //// new code for work with sever
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        handleLoginInfo();
+    ///// new code for work with sever
+        //EditText emailEditText = findViewById(R.id.emailEditText);
+        //EditText passwordEditText = findViewById(R.id.passwordEditText);
+        //Button btn = findViewById(R.id.signupbtn);
+        /*
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,6 +48,51 @@ public class LoginActivity extends AppCompatActivity {
                 String textEmail = emailEditText.getText().toString();
                 String textPassword = passwordEditText.getText().toString();
 
+
+                if (TextUtils.isEmpty(textEmail)){
+                    Toast.makeText(LoginActivity.this, "Будь ласка, введіть пошту", Toast.LENGTH_SHORT).show();
+                    emailEditText.setError("Ви не ввели пошту");
+                    emailEditText.requestFocus();
+                } else if (TextUtils.isEmpty(textPassword)){
+                    Toast.makeText(LoginActivity.this, "Будь ласка, введіть пароль", Toast.LENGTH_SHORT).show();
+                    passwordEditText.setError("Ви не ввели пароль");
+                    passwordEditText.requestFocus();
+                } else {
+                    Intent myint = new Intent(getApplicationContext(), ProfileActivity.class);
+                    startActivity(myint);
+                    //
+                }
+            }
+        });*/
+
+        TextView createNew = findViewById(R.id.createnew);
+        createNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myint = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(myint);
+                //handleLoginDialog
+            }
+        });
+    }
+
+    //// methods for work with server ////
+    private void handleLoginInfo() {
+
+        View view = getLayoutInflater().inflate(R.layout.activity_login, null);
+
+        //AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //builder.setView(view).show();
+
+        Button signipBtn = view.findViewById(R.id.signupbtn);
+        final EditText emailEditText = view.findViewById(R.id.emailEditText);
+        final EditText passwordEditText = view.findViewById(R.id.passwordEditText);
+        String textEmail = emailEditText.getText().toString();
+        String textPassword = passwordEditText.getText().toString();
+
+        signipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 //Check input fields
                 if (TextUtils.isEmpty(textEmail)){
                     Toast.makeText(LoginActivity.this, "Будь ласка, введіть пошту", Toast.LENGTH_SHORT).show();
@@ -41,17 +105,43 @@ public class LoginActivity extends AppCompatActivity {
                 } else {
                     Intent myint = new Intent(getApplicationContext(), ProfileActivity.class);
                     startActivity(myint);
+                    //
                 }
+
+                HashMap<String, String> map = new HashMap<>();
+
+                map.put("email", textEmail);
+                map.put("password", textPassword);
+
+                Call<LoginInfo> call = retrofitInterface.executeLogin(map);
+                call.enqueue(new Callback<LoginInfo>() {
+                    @Override
+                    public void onResponse(Call<LoginInfo> call, Response<LoginInfo> response) {
+
+                        if (response.code() == 200) {
+
+                            LoginInfo result = response.body();
+                            //AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                            //builder1.setTitle(result.getName());
+                            //builder1.setMessage(result.getEmail());
+                            //builder1.show();
+
+                        } else if (response.code() == 404) {
+                            Toast.makeText(LoginActivity.this, "Wrong Credentials",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginInfo> call, Throwable t) {
+                        Toast.makeText(LoginActivity.this, t.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+
             }
         });
 
-        TextView createNew = findViewById(R.id.createnew);
-        createNew.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myint = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivity(myint);
-            }
-        });
     }
 }
