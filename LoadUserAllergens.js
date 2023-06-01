84d85e48b7ad4bfe301664cce7de19d24521e658
malma <mariam.almakadma@nure.ua>
2023-06-01 21:44:46 +0200

    load user allergens checked print

--- LoadUserAllergens.js
+++ LoadUserAllergens.js
@@ -12,17 +12,16 @@ app.use(bodyParser.json());
 
 app.post('/loadUserAllergens', (req, res) => {
     const user_id = req.body.user_id; // Отримання user_id з тіла запиту
-    const query = 'SELECT allergen_name, FROM Allergens ' +
+    const query = 'SELECT allergen_name FROM Allergens ' +
         'inner join UserAllergens on Allergens.allergen_id = UserAllergens.allergen_id ' +
         'inner join Users on UserAllergens.user_id = Users.user_id WHERE Users.user_id = ?;';
 
-    db.get(query, [user_id], (err, rows) => { // Використовуйте db.all замість db.each для отримання всіх рядків
+    db.all(query, [user_id], (err, rows) => { // Використовуйте db.all замість db.each для отримання всіх рядків
         if (err) {
             console.error(err);
             return res.status(500).send('Error retrieving notes from the database');
         }
         res.status(200).json(rows);
-        //res.status(200).json(row);// я не знаю, как лучше, надо потестить!!!!!!!!!!!!!!!!!!!!!!!!
 
     });
 });
