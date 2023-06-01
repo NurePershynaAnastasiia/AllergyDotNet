f4e341a1d0d4bc64817e78d2217e9c9c938053ff
malma <mariam.almakadma@nure.ua>
2023-06-01 16:15:04 +0200

    update DoctorRegistration.js

--- DoctorRegistration.js
+++ DoctorRegistration.js
@@ -3,50 +3,38 @@ const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 const app = express();
 const bodyParser = require('body-parser');
-const db = new sqlite3.Database('AllergyDotNet.db');// Підключення до бази даних SQLite
-const query = `INSERT INTO Doctors (doctor_name, doctor_email, doctor_password, doctor_IBAN, doctor_documents)
-               VALUES ($doctor_name, $doctor_email, $doctor_password, $doctor_IBAN, $doctor_documents)`;
+const db = new sqlite3.Database('AllergyDotNet.db');
 
-// Розбір даних у форматі JSON
+// Parse request bodies
 app.use(express.json());
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
-    //fs.readFile(`newDoctor.json`, 'utf8', async (err, data) => {
-        if (err) {
-            return res.status(500).json({error: 'Error reading the file'});
-        }
-        try {
-            const doctorData = JSON.parse(data);
-            if (doctorData.doctor_password == doctorData.doctor_password1) {//remove
-                // Insert the data into the Users table
-                db.run(query, {
-                    $doctor_name: doctorData.doctor_name,
-                    $doctor_email: doctorData.doctor_email,
-                    $doctor_password: doctorData.doctor_password,
-                    $doctor_IBAN: doctorData.doctor_IBAN,
-                    $doctor_documents: doctorData.doctor_documents
-                }, function (err) {
-                    if (err) {
-                        console.error(err);
-                        return res.status(500).send('Error inserting data into the database');
-                    }
-                });
-                res.json({message: 'Data downloaded and inserted successfully'});
-            } else {
-                res.status(400).json({error: 'Invalid password'});
-            }
+const query = `INSERT INTO Doctors (doctor_name, doctor_email, doctor_password, doctor_IBAN, doctor_documents)
+               VALUES (?, ?, ?, ?, ?)`;
 
-        } catch (error) {
-            res.status(400).json({error: 'Invalid JSON file'});
-        }
-    });
+app.post('/', (req, res) => {
+    const doctor_name = req.body.doctor_name;
+    const doctor_email = req.body.doctor_email;
+    const doctor_password = req.body.doctor_password;
+    const doctor_IBAN = req.body.doctor_IBAN;
+    const doctor_documents = req.body.doctor_documents;
+    const doctor_password1 = req.body.doctor_password1;
+
+    if (doctor_password === doctor_password1) {
+        db.run(query, [doctor_name, doctor_email, doctor_password, doctor_IBAN, doctor_documents], function (err) {
+            if (err) {
+                console.error(err);
+                return res.status(500).send('Error inserting data into the database');
+            }
+            res.json({ message: 'Data downloaded and inserted successfully' });
+        });
+    } else {
+        res.status(400).json({ error: 'Invalid password' });
+    }
 });
 
-// Запуск сервера
-app.use(express.static(__dirname));
+// Start the server
 app.listen(3000, () => {
-    console.log('Сервер запущено на порті 3000');
+    console.log('Server is running on port 3000');
 });
