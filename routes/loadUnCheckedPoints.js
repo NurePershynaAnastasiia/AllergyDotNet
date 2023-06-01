b04cbd475286c811980b817134595ef6faa0f7f8
malma <mariam.almakadma@nure.ua>
2023-06-02 00:23:30 +0200

    load unchecked points file

--- routes/loadUnCheckedPoints.js
+++ routes/loadUnCheckedPoints.js
@@ -3,13 +3,13 @@ const sqlite3 = require('sqlite3').verbose();
 const router = express.Router();
 const db = new sqlite3.Database('AllergyDotNet.db');
 
-router.post('/loadAllCheckedPoints', (req, res) => {
-    const query = 'SELECT point_name, point_photo, point_coordinates_latitude, point_coordinates_longitude FROM Points WHERE point_status = 1';
+router.post('/loadUnCheckedPoints', (req, res) => {
+    const query = 'SELECT point_name, point_photo, point_coordinates_latitude, point_coordinates_longitude FROM Points WHERE point_status = 0';
 
     db.all(query,  (err, rows) => {
         if (err) {
             console.error(err);
-            return res.status(500).send('Error retrieving checked points from the database');
+            return res.status(500).send('Error retrieving unchecked points from the database');
         }
         res.json(rows);
     });
