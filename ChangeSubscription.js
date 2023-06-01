032e05f9a26c793cf8aff072c59289af198eaee4
malma <mariam.almakadma@nure.ua>
2023-06-01 21:58:43 +0200

    change sum 0->1

--- ChangeSubscription.js
+++ ChangeSubscription.js
@@ -10,22 +10,39 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const query = `INSERT INTO Users (user_sub) VALUES (?)`;
+const querySelect = `SELECT user_sub FROM Users WHERE user_id = ?`;
+const queryUpdate = `UPDATE Users SET user_sub = 1 WHERE user_id = ? AND user_sub = 0`;
 
 app.post('/changeUserSub', (req, res) => {
-    const user_sub = req.body.user_sub;
+    const user_id = req.body.user_id;
 
-    if (user_sub != row.user_sub) {
-        db.run(query, [user_sub], function (err) {
+    // Check the current user_sub value
+    db.get(querySelect, [user_id], function (err, row) {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error querying the database');
+        }
+
+        if (!row) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const userSub = row.user_sub;
+
+        if (userSub === 1) {
+            return res.status(200).json({ message: 'Subscription is already active' });
+        }
+
+        // Update the user_sub value
+        db.run(queryUpdate, [user_id], function (err) {
             if (err) {
                 console.error(err);
-                return res.status(500).send('Error inserting data into the database');
+                return res.status(500).send('Error updating data in the database');
             }
-            res.status(200).json({ message: 'User subscription changes sucsesfully!' });
+
+            res.status(200).json({ message: 'User subscription changed successfully!' });
         });
-    } else {
-        res.status(404).json({ error: 'User already had this type of subscription' });
-    }
+    });
 });
 
 // Start the server
