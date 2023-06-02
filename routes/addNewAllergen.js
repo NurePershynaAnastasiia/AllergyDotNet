1b16a65ca2b411594e2c1e87162aae17fc7188c6
malma <mariam.almakadma@nure.ua>
2023-06-02 12:34:35 +0200

    add new allergen (now have if already exist)

--- routes/addNewAllergen.js
+++ routes/addNewAllergen.js
@@ -7,34 +7,39 @@ const querySelect = 'SELECT allergen_id FROM Allergens WHERE allergen_name = ?;'
 const queryInsertAllergens = `INSERT INTO UserAllergens (user_id, allergen_id)
                               VALUES (?, ?)`;
 router.post('/addNewAllergen', (req, res) => {
-    const allergen_name = req.body.allergen_name; // Отримання user_id з тіла запиту
+    const allergen_name = req.body.allergen_name;
     const user_id = req.body.user_id;
 
-    db.get(querySelect, [allergen_name], (err, rows) => {
+    // Перевірка, чи існує вже така пара значень в таблиці UserAllergens
+    const queryCheckExists = `SELECT * FROM UserAllergens WHERE user_id = ? AND allergen_id = 
+                             (SELECT allergen_id FROM Allergens WHERE allergen_name = ?);`;
+    db.get(queryCheckExists, [user_id, allergen_name], (err, row) => {
         if (err) {
             console.error(err);
-            return res.status(500).send('Error retrieving notes from the database');
-        } else if (!row) {
-            return res.status(404).json({message: 'Allergen not found'});
+            return res.status(500).send('Error retrieving data from the database');
+        }
+        if (row) {
+            return res.status(400).json({message: 'This user already has this allergy'});
         } else {
-            res.status(200).json(rows);
-            const allergen_id = rows.allergen_id;
-            db.run(queryInsertAllergens, [user_id, allergen_id], (err, rows) => {
+            // Запис нової алергії в таблицю UserAllergens
+            db.get(querySelect, [allergen_name], (err, rows) => {
                 if (err) {
                     console.error(err);
-                    return res.status(500).send('Error updating data in the database');
+                    return res.status(500).send('Error retrieving allergen from the database');
+                } else if (!rows) {
+                    return res.status(404).json({message: 'Allergen not found'});
+                } else {
+                    const allergen_id = rows.allergen_id;
+                    db.run(queryInsertAllergens, [user_id, allergen_id], (err) => {
+                        if (err) {
+                            console.error(err);
+                            return res.status(500).send('Error updating data in the database');
+                        }
+                        res.status(200).json({message: 'Allergen added successfully!'});
+                    });
                 }
-                res.status(200).json({message: 'Allergen added successfully!'});
-            })
+            });
         }
-    })
-    db.all(query, [allergen_name], (err, rows) => { // Використовуйте db.all замість db.each для отримання всіх рядків
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Error retrieving notes from the database');
-        }
-        res.status(200).json(rows);
-
     });
 });
 
