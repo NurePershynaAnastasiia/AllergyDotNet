74fe578faf7ff25b1142e6d5b1a8b968eabff701
malma <mariam.almakadma@nure.ua>
2023-06-02 13:26:27 +0200

    update and remove point name

--- routes/createPoint.js
+++ routes/createPoint.js
@@ -3,26 +3,35 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('AllergyDotNet.db'); // Підключення до бази даних SQLite
 const router = express.Router();
 
-const query = `INSERT INTO Points (point_name, point_photo, point_info, point_coordinates_latitude, point_coordinates_longitude, allergen_id)
-               VALUES (?, ?, ?, ?, ?, ?)`;
+const querySelect = 'SELECT allergen_id FROM Allergens WHERE allergen_name = ?;';
+const query = `INSERT INTO Points (point_photo, point_info, point_coordinates_latitude, point_coordinates_longitude, allergen_id)
+               VALUES (?, ?, ?, ?, ?)`;
 
-router.post('/', (req, res) => {
-
-        const point_name = req.body.point_name;
-        const point_photo = req.body.point_photo;
-        const point_info = req.body.point_info;
-        const point_coordinates_latitude = req.body.point_coordinates_latitude;
-        const point_coordinates_longitude = req.body.point_coordinates_longitude;
-        const allergen_id = req.body.allergen_id;
+router.post('/createPoint', (req, res) => {
+    const allergen_name = req.body.allergen_name;
+    const point_info = req.body.point_info;
+    const point_photo = req.body.point_photo;
+    const point_coordinates_latitude = req.body.point_coordinates_latitude;
+    const point_coordinates_longitude = req.body.point_coordinates_longitude;
 
         // Insert the data into the Points table
-        db.run(query, [point_name, point_photo, point_info, point_coordinates_latitude, point_coordinates_longitude, allergen_id], function (err){
-            if (err) {
-                console.error(err);
-                return res.status(500).send('Error inserting data into the database');
-            }
-            res.json({ message: 'Data downloaded and inserted successfully' });
-        });
+    db.get(querySelect, [allergen_name], (err, rows) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error retrieving allergen from the database');
+        } else if (!rows) {
+            return res.status(404).json({message: 'Allergen not found'});
+        } else {
+            const allergen_id = rows.allergen_id;
+            db.run(query, [point_photo, point_info, point_coordinates_latitude, point_coordinates_longitude, allergen_id], (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).send('Error updating data in the database');
+                }
+                res.status(200).json({message: 'Point added successfully!'});
+            });
+        }
+    });
 
 });
 
