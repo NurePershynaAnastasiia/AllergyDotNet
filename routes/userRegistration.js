c3815d31d0eac34f11ea1c30a6eded43db92e7d7
Anastasiia <nastysha.001.99@gmail.com>
2023-06-02 20:40:13 +0300

    server 5.8 update pass valid amd user Register send id

--- routes/userRegistration.js
+++ routes/userRegistration.js
@@ -3,8 +3,9 @@ const sqlite3 = require('sqlite3').verbose();
 const router = express.Router();
 const db = new sqlite3.Database('AllergyDotNet.db');
 
-const query = `INSERT INTO Users (user_name, user_email, user_password)
+const queryInsert = `INSERT INTO Users (user_name, user_email, user_password)
                VALUES (?, ?, ?)`;
+const querySelect = `SELECT user_id FROM Users WHERE user_email = ? AND user_name = ?`;
 
 router.post('/userRegistration', (req, res) => {
     const user_name = req.body.user_name;
@@ -12,12 +13,19 @@ router.post('/userRegistration', (req, res) => {
     const user_password = req.body.user_password;
     const user_password1 = req.body.user_password1;
 
-        db.run(query, [user_name, user_email, user_password], function (err) {
+        db.run(queryInsert, [user_name, user_email, user_password], function (err) {
             if (err) {
                 console.error(err);
                 return res.status(500).send('Error inserting data into the database');
+            }else{
+                db.get(querySelect, [user_email, user_name], function (err) {
+                    if(err){
+                        return res.status(500).send('Error inserting data into the database');
+                    }else{
+                        res.status(200).json({user_id: row.user_id });
+                    }
+                })
             }
-            res.json({ message: 'Data downloaded and inserted successfully' });
         });
 
 });
