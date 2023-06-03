8171b441a33b850269b6ca923a2597e382bcadda
malma <mariam.almakadma@nure.ua>
2023-06-03 17:10:16 +0200

    doctor log

--- routes/doctorLogIn.js
+++ routes/doctorLogIn.js
@@ -5,24 +5,25 @@ const db = new sqlite3.Database('AllergyDotNet.db');// Підключення д
 
 router.post('/doctorLogin', (req, res) => {
     const doctor_email = req.body.doctor_email;
-    const doctor_password = req.body.doctor_email;
+    const doctor_password = req.body.doctor_password;
 
-    db.get('SELECT doctor_id, doctor_password, isAdmin FROM Doctors WHERE doctor_email = ?', [doctor_email], (err, row) => {
+    db.get('SELECT doctor_id, doctor_password, isAdmin FROM Doctors WHERE doctor_email = ?', [doctor_email], (err, rows) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Error retrieving data from the database');
         }
-        if (row) {
-            if (row.doctor_password === doctor_password) {
-                if (row.isAdmin) {
-                    res.status(200).json({doctor_id: row.doctor_id, isAdmin: true});//доктор
+        if (rows) {
+           if (rows.doctor_password === doctor_password) {
+                if (rows.isAdmin) {
+                    res.status(200).json({doctor_id: rows.doctor_id, isAdmin: true});//адмін
 
                 } else {
-                    res.status(200).json({doctor_id: row.doctor_id, isAdmin: false});//адмін
+                    res.status(200).json({doctor_id: rows.doctor_id, isAdmin: false});//доктор
                 }
             } else {
                 res.status(404).json({error: 'Invalid password'});//якщо пароль невірний
             }
+            res.status(200).json(rows);
         } else {
             res.status(200).json({doctor_id: false});//людини з таким емайлом нема
         }
