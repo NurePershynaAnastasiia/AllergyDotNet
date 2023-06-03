f159f81fdd50b58aab25eca9938aaa373ccaa4ee
malma <mariam.almakadma@nure.ua>
2023-06-03 17:14:54 +0200

    load doc 2

--- routes/loadDoctorMobile.js
+++ routes/loadDoctorMobile.js
@@ -7,7 +7,7 @@ router.post('/loadDoctorMobile', (req, res) => {
     const doctor_id = req.body.doctor_id; // Отримання user_id з тіла запиту
     const query = 'SELECT doctor_name, doctor_photo, doctor_price, doctor_info FROM Doctors WHERE doctor_id = ?';
 
-    db.all(query, [doctor_id], (err, rows) => {
+    db.get(query, [doctor_id], (err, rows) => {
         if (err) {
             console.error(err);
             return res.status(500).send('Error retrieving notes from the database');
